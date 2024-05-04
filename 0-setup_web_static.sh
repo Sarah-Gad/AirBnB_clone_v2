8452765ef1d8489fbae87a5dbd90e03363298acb
Sarah-Gad <sarahgad298@gmail.com>
2024-05-04 05:09:50 +0300

    exe .-.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,1 +1,22 @@
-./
+#!/usr/bin/env bash
+# This script sets up my web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i '28i\    location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+sudo service nginx restart
