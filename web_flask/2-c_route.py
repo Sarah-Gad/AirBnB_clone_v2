441eadb18fdce6b43f610d40ecda0f639bdf9b82
Sarah-Gad <sarahgad298@gmail.com>
2024-05-18 22:48:12 +0300

    completed task n.2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This script will start a flask web application"""
+from flask import Flask
+my_airbnb_app = Flask(__name__)
+
+
+@my_airbnb_app.route("/", strict_slashes=False)
+def hellopage():
+    """This fucntion will be called when the user access the root url /"""
+    return "Hello HBNB!"
+
+
+@my_airbnb_app.route("/hbnb")
+def hbnbpage():
+    """This fucntion will be called when the user access this url /hbnb"""
+    return "HBNB"
+
+
+@my_airbnb_app.route("/c/<text>")
+def c_page(text):
+    """This function will be called when the used access this url /c/<text>"""
+    converted_txt = text.replace('_', ' ')
+    return ("c {}".format(converted_txt))
+
+
+if __name__ == "__main__":
+    my_airbnb_app.run(host='0.0.0.0', port=5000)
