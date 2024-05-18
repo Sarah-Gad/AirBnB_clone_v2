481b276c8dad8a203228925d5fbcdffd7d98e5e5
Sarah-Gad <sarahgad298@gmail.com>
2024-05-18 21:55:39 +0300

    pycodestyle second trial

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This script will start a flash web application"""
+
 from flask import Flask
 my_airbnb_app = Flask(__name__)
+
+
 @my_airbnb_app.route("/", strict_slashes=False)
 def hellopage():
     """This function will be called when the user access the root url"""
