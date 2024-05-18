8a2026cef609e89ad365bc6ce4d9b0e982c8c082
Sarah-Gad <sarahgad298@gmail.com>
2024-05-18 22:38:02 +0300

    completed task n.1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script will start a flask web application"""
+from flask import Flask
+my_airbnb_app = Flask(__name__)
+
+
+@my_airbnb_app.route("/", strict_slashes=False)
+def hellopage():
+    """This fucntion will be called when the user access the root url /"""
+    return "Hello HBNB!"
+
+
+@my_airbnb_app.route("/hbnb")
+def hbnbpage():
+    """This fucntion will be called when the user access this url /hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    my_airbnb_app.run(host='0.0.0.0', port=5000)
