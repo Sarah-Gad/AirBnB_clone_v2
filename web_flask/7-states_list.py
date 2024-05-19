c0d3e552dd6dc589319ee541a63246da168a7f56
Sarah-Gad <sarahgad298@gmail.com>
2024-05-19 11:11:20 +0300

    .-.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,22 +2,22 @@
 """This script will start the falsk web application for my HBNB project"""
 from flask import Flask, render_template
 from models import storage
-my_airnbn_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_airnbn_app.route("/states_list", strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def listing_states():
     """This function will be called when the
     user access this url /states_list"""
-    allstates = storage.all("State").values()
-    return render_template("7-states_list.html", passed_states=allstates)
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
-@my_airnbn_app.teardown_appcontext
-def closingst(excep):
+@app.teardown_appcontext
+def teardown(excep):
     """This function will be called at the end of each request"""
     storage.close()
 
 
 if __name__ == '__main__':
-    my_airnbn_app.run(host='0.0.0.0', port=5000)
+   app.run(host='0.0.0.0', port=5000)
