ea83d5849cb70f6cbc182d29889a95b66d8a66aa
Sarah-Gad <sarahgad298@gmail.com>
2024-05-19 11:05:37 +0300

    competed task n.8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This script will start the falsk web application for my HBNB project"""
+from flask import Flask, render_template
+from models import storage
+my_airnbn_app = Flask(__name__)
+
+
+@my_airnbn_app.route("/states_list", strict_slashes=False)
+def listing_states():
+    """This function will be called when the
+    user access this url /states_list"""
+    allstates = storage.all("State").values()
+    return render_template("7-states_list.html", passed_states=allstates)
+
+
+@my_airnbn_app.teardown_appcontext
+def closingst(excep):
+    """This function will be called at the end of each request"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    my_airnbn_app.run(host='0.0.0.0', port=5000)
